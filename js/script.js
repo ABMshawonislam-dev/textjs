6f7110acbb9d70cfbc1a56dbeb944afc9b355d8a
A B M Shawon Islam <abmshawonislam@gmail.com>
2021-02-16 00:46:09 +0600

    margin padding added

--- js/script.js
+++ js/script.js
@@ -14,6 +14,55 @@ abmArr.map(text=>{
               }else if(mainvalue == "size"){
                 let size = value.split("-")[2];
                 text.style.fontSize = size
+              }else if(mainvalue == "bg"){
+                let bg = value.split("-")[2];
+                text.style.background = bg
+              }else if(mainvalue == "p"){
+                let padding = value.split("-")[2];
+                text.style.padding = padding
+              }else if(mainvalue == "pl"){
+                let paddingleft = value.split("-")[2];
+                text.style.paddingLeft = paddingleft
+              }else if(mainvalue == "pr"){
+                let paddingright = value.split("-")[2];
+                text.style.paddingRight = paddingright
+              }else if(mainvalue == "pt"){
+                let paddingtop = value.split("-")[2];
+                text.style.paddingTop = paddingtop
+              }else if(mainvalue == "pb"){
+                let paddingbottom = value.split("-")[2];
+                text.style.paddingBottom = paddingbottom
+              }else if(mainvalue == "px"){
+                let paddingx = value.split("-")[2];
+                text.style.paddingLeft = paddingx
+                text.style.paddingRight = paddingx
+              }else if(mainvalue == "py"){
+                let paddingy = value.split("-")[2];
+                text.style.paddingTop = paddingy
+                text.style.paddingBottom = paddingy
+              }else if(mainvalue == "m"){
+                let margin = value.split("-")[2];
+                text.style.margin = margin
+              }else if(mainvalue == "ml"){
+                let marginleft = value.split("-")[2];
+                text.style.marginLeft = marginleft
+              }else if(mainvalue == "mr"){
+                let marginright = value.split("-")[2];
+                text.style.marginRight = marginright
+              }else if(mainvalue == "mt"){
+                let margintop = value.split("-")[2];
+                text.style.marginTop = margintop
+              }else if(mainvalue == "mb"){
+                let marginbottom = value.split("-")[2];
+                text.style.marginBottom = marginbottom
+              }else if(mainvalue == "mx"){
+                let marginx = value.split("-")[2];
+                text.style.marginLeft = marginx
+                text.style.marginRight = marginx
+              }else if(mainvalue == "my"){
+                let marginy = value.split("-")[2];
+                text.style.marginTop = marginx
+                text.style.marginBottom = marginx
               }
           })
       }
